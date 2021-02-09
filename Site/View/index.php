d575ff472f4f7a5fc8b45a5f196bac0e180a0694
Lucas Alves <lucas1alves98@gmail.com>
2021-02-09 18:54:21 -0300

    Adicionei header e footer

--- Site/View/index.php
+++ Site/View/index.php
@@ -4,9 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
+    <header>
+        <div class="left-header"> 
+
+        </div>
+    <nav class="menu">
+    <ul>
+    <li ><a href="home.php">Home</a></li>
+    <li ><a  href="contato.php">Contato</a></li>
+    <li ><a  href="sobre.php">Sobre</a></li>
+    </ul>
+    </nav>
+    </header>
+    <main>
+        
     
+    </main>
+
+    <footer>
+    <p>Loren ipsum dolor sit amet consectetur adipisicing elit. Nisi eos quidem beatae sint quibusdam voluptatem nam ut molestiae excepturi quos dolor necessitatibus architecto tempora, accusamus obcaecati ullam vel hic? Perferendis?</p>
+        <ul>
+            <li><a href="linkedin.com">Linkedin</a></li>
+            <li><a href="whatsapp">whatsapp</a></li>
+            <li><a href="github">GitHub</a></li>
+        </ul>
+
+    </footer>
 </body>
 </html>
